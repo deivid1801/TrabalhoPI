7b851bf3931951b0f1bad3011520668e3dd430e0
= <=>
2023-04-05 12:18:09 -0300

    atualizaçoes

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <script setup>
 import { ref } from 'vue'
-const texto = ref('')
+const email = ref('')
+const nome = ref('')
+const dataN = ref('')
+const endereço = ref('')
+const cidade = ref('')
+const estado = ref('')
+const Hobbies = ref('')
+const Linguagens = ref('')
+const Biografia = ref('')
+const senha = ref('')
+const confirmaçao = ref('')
 
 
 
@@ -12,16 +22,21 @@ const texto = ref('')
   <form>
   <div class="row">
     <div class="col">
-      <input  type="text" v-bind:value="texto" v-on:input="event => texto = event.target.value" class="form-control" placeholder="Enter email" name="email ">
-      <input type="text" class="form-control" placeholder="insira seu nome">
-      <input type="text" class="form-control" placeholder="insira sua data de nascimento">
-      <input type="text" class="form-control" placeholder="insira seu endereço">
-      <input type="text" class="form-control" placeholder="insira sua cidade">
-      <p>Texto digitado: {{ texto }}</p>
+      <input type="text" v-bind:value="email" v-on:input="event => email = event.target.value" class="form-control"
+          placeholder="Enter email" name="email ">
+        <input type="text" v-bind:value="nome" v-on:input="event => nome = event.target.value" class="form-control"
+          placeholder="insira seu nome">
+        <input type="text" v-bind:value="dataN" v-on:input="event => dataN = event.target.value" class="form-control"
+          placeholder="insira sua data de nascimento">
+        <input type="text" v-bind:value="endereço" v-on:input="event => endereço = event.target.value"
+          class="form-control" placeholder="insira seu endereço">
+        <input type="text" v-bind:value="cidade" v-on:input="event => cidade = event.target.value" class="form-control"
+          placeholder="insira sua cidade">
+     
 
       <p>insira seu Estado</p>
 
-     <select class="form-select mt-3">
+     <select v-bind:value="estado" v-on:input="event => estado = event.target.value" class="form-select mt-3">
      <option>AC</option>
      <option>AL</option>
      <option>AP</option>
@@ -50,22 +65,37 @@ const texto = ref('')
      <option>SE</option>
      <option>TO</option>
    </select>
-   <input type="text" class="form-control" placeholder="insira seus Hobbies
+   <input type="text" v-bind:value="Hobbies" v-on:input="event => Hobbies = event.target.value" class="form-control" placeholder="insira seus Hobbies
 ">
-   <input type="text" class="form-control" placeholder="insira suas Linguagens de programação
+   <input type="text" v-bind:value="Linguagens" v-on:input="event => Linguagens = event.target.value" class="form-control" placeholder="insira suas Linguagens de programação
 ">
-   <input type="text" class="form-control" placeholder="insira sua Biografia">
+   <input type="text" v-bind:value="Biografia" v-on:input="event => Biografia = event.target.value" class="form-control" placeholder="insira sua Biografia">
 
 
     </div>
     <div class="col">
-      <input type="password" class="form-control" placeholder="insira sua senha" name="pswd">
-      <input type="password" class="form-control" placeholder="confirmaçao de senha" name="pswd">
+      <input type="password" v-bind:value="senha" v-on:input="event => senha = event.target.value" class="form-control" placeholder="insira sua senha" name="pswd">
+      <input type="password" v-bind:value="confirmaçao" v-on:input="event => confirmaçao = event.target.value" class="form-control" placeholder="confirmaçao de senha" name="pswd">
 
       
     </div>
   </div>
 </form>
+
+<div><h4>dados usuario</h4>
+    <p>email digitado: {{ email }}</p>
+<p>nome digitado:{{ nome }}</p>
+<p>data de nascimento digitado:{{ dataN }}</p>
+<p>endereço digitado:{{ endereço }}</p>
+<p>cidade digitado:{{ cidade }}</p>
+<p>estado digitado:{{ estado }}</p>
+<p>Hobbies digitado:{{ Hobbies }}</p>
+<p>Linguagens digitado:{{ Linguagens }}</p>
+<p>biografia digitado:{{ Biografia }}</p>
+<p>senha digitado:{{ senha }}</p>
+<p>confirmaçao digitado:{{ confirmaçao }}</p>
+</div>
+
 </template>
 
 <style scoped>
